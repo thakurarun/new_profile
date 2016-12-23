155ef75842808ddebb5e8494519167eb324d9d9c
Arun Thakur <arunk.thakur@basware.com>
2016-12-23 17:03:34 +0530

    initial commit

---
+++ WebApplication/wwwroot/scripts/main.js
@@ -0,0 +1,72 @@
+﻿var app = (function (materialize) {
+    return {
+        init: function () {
+            $(".button-collapse").sideNav();
+
+            $('#menu > .hover-effect > .menu-item').on('mouseover', function () {
+                $(this).addClass('z-depth-5');
+            }).on('mouseleave', function () {
+                $(this).removeClass('z-depth-5');
+            });
+
+            $('#menu li.menu-item').on('click', function (event) {
+                event.preventDefault();
+                event.stopPropagation();
+                $('#menu li.menu-item').removeClass('active');
+                $(this).addClass('active');
+                $("html, body").animate({ scrollTop: $($(this).find('a').attr('href')).offset().top - 70 }, "slow");
+            });
+
+            this.adjustPage();
+            this.scrollFire();
+        },
+
+        scrollToTop: function () {
+            $("html, body").animate({ scrollTop: 0 }, "slow");
+        },
+
+        adjustPage: function () {
+            var menuHeight = $('#menu').height();
+            var availableHeight = screen.height - menuHeight;
+            $('div.full-page').height(availableHeight);
+        },
+
+        getPageHeight: function () {
+            return $('div.full-page').height();
+        },
+
+        scrollFire: function () {
+            var self = this;
+            var halfHeight = self.getPageHeight() / 2;
+            materialize.toast("Check my profile!", self.getPageHeight() * 1);
+            var options = [
+                 {
+                     selector: '#work', offset: halfHeight, callback: function () {
+                         materialize.toast("See my work!", self.getPageHeight() * 2);
+                     }
+                 }, {
+                     selector: '#resume', offset: halfHeight, callback: function () {
+                         materialize.toast("Download my resume!", self.getPageHeight() * 3);
+                     }
+                 }, {
+                     selector: '#contact', offset: halfHeight, callback: function () {
+                         materialize.toast("Get in contact!", self.getPageHeight() * 4);
+                     }
+                 }
+            ];
+            materialize.scrollFire(options);
+        }
+
+    }
+})(Materialize);
+
+
+$(document).ready(function () {
+    app.scrollToTop();
+    app.init();
+
+});
+$(window).resize(function () {
+    app.adjustPage();
+
+});
